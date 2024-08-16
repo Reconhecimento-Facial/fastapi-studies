3595759c5fa00ee1bece963907234a67c34c89da
Vinícius Cabral <vinicius.alcantara@edge.ufal.br>
2024-08-16 19:09:19 -0300

    Criação de um teste para a criação de usuário. Começei a usar uma fixture para não repetir o cliente de teste

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,32 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-client = TestClient(app)
-
-
-def test_read_root_deve_retornar_ok_e_hello_world():
+def test_read_root_deve_retornar_ok_e_hello_world(client):
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'bem vindo a raiz do seu servidor'}
 
 
-def test_read_ola_da_web_deve_retornar_ok_e_ola_mundo():
+def test_read_ola_da_web_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/ola_da_web')
     print(response.text)
     assert response.status_code == HTTPStatus.OK
     assert 'Olá Mundo!' in response.text
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'lucas',
+            'email': 'lucas@exemplo.com',
+            'password': '123456',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'lucas',
+        'email': 'lucas@exemplo.com',
+    }
